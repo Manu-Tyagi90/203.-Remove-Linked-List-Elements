76452c51a3ae88e236728acada9d88fe33893847
Manu Tyagi <manutygii90@gmail.com>
2025-02-07 18:30:43 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,36 +1,41 @@
-class ListNode{
-int val;
-ListNode next;
-ListNode(int val) {
-  this.val = val;
-}
-ListNode(int val,ListNode next) {
-  this.val = val;
-  this.next = next;
-}
+class ListNode {
+    int val;
+    ListNode next;
+    
+    ListNode(int val) {
+        this.val = val;
+    }
+    
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
 
 public class Solution {
-  private static ListNode removeElements(ListNode l1, int val) {
-    ListNode dummy = new ListNode(0);
-    dummy.next = l1;
-    if(l1==null) return null;
-    while(l1.next!=null) {
-      if(l1.next.val == val) {
-        l1.next = l1.next.next;
-      }
-      else{
-        l1 = l1.next;
-      }
-    }return dummy.next;
-  }
+    private static ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+        
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        
+        return dummy.next;
+    }
 
-public static void main(String[] manu) {
-  ListNode l1 = new ListNode(1,new ListNode(2,new ListNode(3, new ListNode(4))));
-  ListNode rm = removeElements(l1,3);
-  while(l1!=null) {
-    System.out.print(l1.val+" ");  
-    l1=l1.next;
-}
-}
+    public static void main(String[] manu) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+        ListNode rm = removeElements(l1, 3);
+        
+        while (rm != null) {
+            System.out.print(rm.val + " ");
+            rm = rm.next;
+        }
+    }
 }
